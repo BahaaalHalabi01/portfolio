9d12d1043085f2c64d8a592417ac2e52aaba36cc
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2023-12-19 02:22:42 +0300

    basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,71 @@
 <script>
-  import "../app.css";
+	import Telegram from '$src/lib/icons/telegram.svelte';
+	import '../app.css';
+	import { BugPlay, Github, Instagram, Linkedin, Twitter } from 'lucide-svelte';
+
+	let data;
+
+	const headers = [
+		{ href: '/about', label: 'about' },
+		{ href: '/experience', label: 'experience' },
+		{ href: '/work', label: 'work' },
+		{ href: '/contact', label: 'contact' }
+	];
 </script>
 
-<slot />
+<main class="container w-full mx-auto flex flex-col grow min-h-screen relative">
+	<nav class="flex items-center justify-between h-16 font-bold text-lg capitalize sticky top-0 bg-slate-900">
+		<span class="inline-flex gap-x-2 items-center p-2">
+			<BugPlay />
+			Bahaa al Halabi
+		</span>
+		<div class="flex flex-row lg:gap-x-12 items-center">
+			{#each headers as header, i}
+				<span class="flex gap-x-2">
+					<span class="text-green-600">0{i + 1}{'.'}</span>
+					<a href={header.href} class="cursor-pointer">
+						{header.label}
+					</a>
+				</span>
+			{/each}
+
+			<a class="border-green-600 border-2 p-2" href="/resume">Checkout My Resume!</a>
+		</div>
+	</nav>
+	<div class="lg:px-24 py-12">
+		<slot />
+	</div>
+
+	<div class="sticky left-0 bottom-0 flex flex-col gap-y-8 lg:pb-24 max-w-fit bg-slate-900">
+		<a
+			href="https://github.com/BahaaalHalabi01"
+			class="hover:scale-125 transition-transform duration-300 ease-out"
+			target="_blank"
+			rel="author"
+		>
+			<Github />
+		</a>
+		<a
+			href="https://www.linkedin.com/in/bahaa-al-halabi-78066a1a1"
+			target="_blank"
+			rel="author"
+			class="hover:scale-125 transition-transform duration-300 ease-out"
+		>
+			<Linkedin />
+		</a>
+		<a
+			class="hover:scale-125 transition-transform duration-300 ease-out"
+			href="https://t.me/bahaaalhalabi01"
+			target="_blank"
+			rel="author"
+		>
+			<Telegram />
+		</a>
+		<a href="/" class="hover:scale-125 transition-transform duration-300 ease-out">
+			<Instagram />
+		</a>
+		<a href="/" class="hover:scale-125 transition-transform duration-300 ease-out">
+			<Twitter />
+		</a>
+	</div>
+</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    container: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
+        '3xl': '1792px'
+      },
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        lg: '2rem',
+        xl: '3rem',
+        '2xl':'5rem',
+        '3xl': '5rem'
+      },
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['"EB Garamond"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'svelte': '#ff4700'
+      }
+    },
   },
   plugins: [],
 }
