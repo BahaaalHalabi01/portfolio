a30c7be523bb22bd56a3428c3f826357352600cc
Bahaa al Halabi <67575710+BahaaalHalabi01@users.noreply.github.com>
2023-12-21 02:33:37 +0300

    responsive header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,15 +16,20 @@
 
 <main class="container w-full mx-auto flex flex-col grow min-h-screen relative">
 	<nav
-		class="flex items-center justify-between h-16 font-bold text-lg capitalize sticky top-0 bg-slate-900 z-20"
+		class="lg:flex items-center justify-between lg:h-16 font-bold text-lg capitalize sticky top-0 bg-slate-900 z-20 lg:py-0 py-2"
 	>
 		<span class="inline-flex gap-x-2 items-center p-2">
 			<BugPlay />
 			Bahaa al Halabi
 		</span>
-		<div class="flex flex-row lg:gap-x-12 items-center">
+
+		<a class="border-green-600 border-2 p-2 inline-block lg:hidden float-right" href="/resume"
+			>My Resume!</a
+		>
+
+		<div class="flex flex-row lg:gap-x-12 items-center lg:py-0 py-2 text-sm lg:text-base justify-between lg:w-fit w-full">
 			{#each headers as header, i}
-				<span class="flex gap-x-2">
+				<span class="flex lg:gap-x-2 gap-x-1">
 					<span class="text-green-600">0{i + 1}{'.'}</span>
 					<a href={header.href} class="cursor-pointer">
 						{header.label}
@@ -32,14 +37,15 @@
 				</span>
 			{/each}
 
-			<a class="border-green-600 border-2 p-2" href="/resume">Checkout My Resume!</a>
+			<a class="border-green-600 border-2 p-2 hidden lg:block" href="/resume">Checkout My Resume!</a
+			>
 		</div>
 	</nav>
-	<div class="lg:px-24 py-12 snap-both snap-always">
+	<div class="lg:px-24 lg:py-12 py-4">
 		<slot />
 	</div>
 
-	<div class="sticky left-0 bottom-0 flex flex-col gap-y-8 lg:pb-24 max-w-fit bg-slate-900">
+	<div class="md:sticky hidden left-0 bottom-0 md:flex flex-col gap-y-8 lg:pb-24 max-w-fit bg-slate-900">
 		<a
 			href="https://github.com/BahaaalHalabi01"
 			class="hover:scale-125 transition-transform duration-300 ease-out"
